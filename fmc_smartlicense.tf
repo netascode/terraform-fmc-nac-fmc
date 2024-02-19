59c79adfbc414797de57987dd529a1f5999360e9
mmaciejc@cisco.com <mmaciejc@cisco.com>
2024-02-19 09:09:31 +0100

    initial release

--- fmc_smartlicense.tf
+++ fmc_smartlicense.tf
@@ -5,10 +5,10 @@ resource "fmc_smart_license" "license" {
   count = can(local.model.fmc.smart_license) ? 1 : 0
 
   # Mandatory
-  registration_type = try(local.model.fmc.smartlicense.registration_type, null)
+  registration_type = try(local.model.fmc.smart_license.registration_type, null)
 
   # Optional
-  token  = try(local.model.fmc.smartlicense.token, null)
-  retain = try(local.model.fmc.smartlicense.retain, local.defaults.fmc.smartlicense.retain, null)
-  force  = try(local.model.fmc.force, local.defaults.fmc.smartlicense.force, null)
+  token  = try(local.model.fmc.smart_license.token, null)
+  retain = try(local.model.fmc.smart_license.retain, local.defaults.fmc.smart_license.retain, null)
+  force  = try(local.model.fmc.force, local.defaults.fmc.smart_license.force, null)
 }
