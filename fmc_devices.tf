7e958a29662354d2d3c60d45bd788e273bdb0cfc
danischm <danischm@cisco.com>
2024-11-11 21:15:26 +0100

    Latest schema updates

--- fmc_devices.tf
+++ fmc_devices.tf
@@ -116,9 +116,9 @@ resource "fmc_device_physical_interfaces" "physical_interface" {
   # Optional
   if_name                = try(each.value.data.name, null)
   security_zone_id       = try(local.map_securityzones[each.value.data.security_zone].id, null)
-  enabled                = try(each.value.data.enabled, local.defaults.fmc.domains.devices.physical_interfaces.enabled)
+  enabled                = try(each.value.data.enabled, local.defaults.fmc.domains.devices.devices.physical_interfaces.enabled)
   mtu                    = try(each.value.data.mtu, null)
-  mode                   = try(each.value.data.mode, local.defaults.fmc.domains.devices.physical_interfaces.mode)
+  mode                   = try(each.value.data.mode, local.defaults.fmc.domains.devices.devices.physical_interfaces.mode)
   ipv4_static_address    = try(each.value.data.ipv4_static_address, null)
   ipv4_static_netmask    = try(each.value.data.ipv4_static_netmask, null)
   ipv4_dhcp_enabled      = try(each.value.data.ipv4_dhcp, null)
@@ -126,7 +126,7 @@ resource "fmc_device_physical_interfaces" "physical_interface" {
   ipv6_address           = try(each.value.data.ipv6_address, null)
   ipv6_prefix            = try(each.value.data.ipv6_prefix, null)
   ipv6_enforce_eui       = try(each.value.data.ipv6_enforce_eui64, null)
-  description            = try(each.value.data.description, local.defaults.fmc.domains.devices.physical_interfaces.description, null)
+  description            = try(each.value.data.description, local.defaults.fmc.domains.devices.devices.physical_interfaces.description, null)
 
   depends_on = [
     data.fmc_device_physical_interfaces.physical_interface,
@@ -145,7 +145,7 @@ resource "fmc_device_physical_interfaces" "physical_interface" {
 locals {
   res_sub_interface = flatten([
     for domain in local.domains : [
-      for device in try(domain.devices, []) : [
+      for device in try(domain.devices.devices, []) : [
         for physicalinterface in try(device.physical_interfaces, []) : [
           for subinterface in try(physicalinterface.subinterfaces, []) : {
             key          = "${device.name}/${physicalinterface.interface}/${subinterface.id}"
@@ -181,7 +181,7 @@ resource "fmc_device_subinterfaces" "sub_interfaces" {
   ipv6_enforce_eui       = try(each.value.data.ipv6_enforce_eui, null)
   ipv6_prefix            = try(each.value.data.ipv6_prefix, null)
   management_only        = try(each.value.data.management_only, null)
-  mode                   = try(each.value.data.mode, local.defaults.fmc.domains.devices.physical_interfaces.subinterfaces.mode, null)
+  mode                   = try(each.value.data.mode, local.defaults.fmc.domains.devices.devices.physical_interfaces.subinterfaces.mode, null)
   mtu                    = try(each.value.data.mtu, null)
   priority               = try(each.value.data.priority, null)
   security_zone_id       = try(local.map_securityzones[each.value.data.security_zone].id, null)
@@ -194,7 +194,7 @@ locals {
 
   res_vtep_interfaces = flatten([
     for domain in local.domains : [
-      for device in try(domain.devices, []) : [
+      for device in try(domain.devices.devices, []) : [
         for physicalinterface in try(device.physical_interfaces, []) : [
           for vtep in try(physicalinterface.vteps, []) : {
             key                  = "${device.name}/${physicalinterface.interface}/${vtep.encapsulation_port}"
@@ -250,7 +250,7 @@ resource "fmc_device_vni" "vni" {
   security_zone_id       = try(local.map_securityzones[each.value.data.security_zone].id, null)
   if_name                = each.value.data.name
   enabled                = try(each.value.data.enabled, null)
-  description            = try(each.value.data.description, local.defaults.fmc.domains.devices.vnis.description, "VNI Interface")
+  description            = try(each.value.data.description, local.defaults.fmc.domains.devices.devices.vnis.description, "VNI Interface")
   priority               = try(each.value.data.priority, null)
   vnid                   = each.value.data.vni_id
   segment_id             = try(each.value.data.vni_segment_id, null)
@@ -285,9 +285,9 @@ locals {
         for ipv4staticroute in try(device.ipv4_static_routes, []) : {
           key               = "${device.name}/${ipv4staticroute.name}"
           device_id         = local.map_devices[device.name].id
-          gateway_id        = local.map_networkobjects[ipv4staticroute.gateway].id
-          gateway_type      = local.map_networkobjects[ipv4staticroute.gateway].type
-          gateway_name      = ipv4staticroute.gateway
+          gateway_id        = local.map_networkobjects[ipv4staticroute.gateway.object].id
+          gateway_type      = local.map_networkobjects[ipv4staticroute.gateway.object].type
+          gateway_name      = ipv4staticroute.gateway.object
           interface_name    = ipv4staticroute.interface
           selected_networks = ipv4staticroute.selected_networks
         }
@@ -321,7 +321,7 @@ resource "fmc_staticIPv4_route" "ipv4staticroute" {
   }
 
   # Optional
-  is_tunneled = try(each.value.tunneled, local.defaults.fmc.domains.devices.ipv4_static_routes.tunneled, null)
+  is_tunneled = try(each.value.tunneled, local.defaults.fmc.domains.devices.devices.ipv4_static_routes.tunneled, null)
 
   depends_on = [
     fmc_device_physical_interfaces.physical_interface,
